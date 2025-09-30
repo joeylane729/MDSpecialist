7faecf2fb6a1f148b27dde623c112eab9ea03fe6
joeylane729 <jophilane@gmail.com>
2025-09-29 22:05:39 -0500

    Force Railway to use Docker build
    - Update railway.toml to explicitly force Docker
    - Add [build.docker] section
    - Remove .railwayignore approach

--- backend/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Start script for Railway deployment
-
-# Set default port if not provided
-export PORT=${PORT:-8000}
-
-# Start the application
-uvicorn main:app --host 0.0.0.0 --port $PORT
