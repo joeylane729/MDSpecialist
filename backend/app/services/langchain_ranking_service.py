5df2ad90660d8462d4aed82252accd84f71a991c
joeylane729 <jophilane@gmail.com>
2025-10-20 15:55:10 -0500

    Update LLMChain import to use specific langchain.chains.llm module

--- backend/app/services/langchain_ranking_service.py
+++ backend/app/services/langchain_ranking_service.py
@@ -10,7 +10,7 @@ import time
 from typing import List, Dict, Any, Optional
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains.llm import LLMChain
 from ..models.specialist_recommendation import SpecialistRecommendation
 from sqlalchemy.orm import Session
 from sqlalchemy import text
