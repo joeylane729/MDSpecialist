79085888b7fa29216ae984755b543e4141e5f361
joeylane729 <jophilane@gmail.com>
2025-11-30 13:39:05 -0500

    Include diagnoses_prompt_text in searchParams when navigating to ResultsPage
    - Add diagnoses_prompt_text to searchParams in HomePage when passing data to ResultsPage
    - Ensures the diagnoses/treatment options prompt is visible in the UI after initial search

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -679,6 +679,7 @@ const HomePage: React.FC = () => {
           treatment_options: aiRecommendations?.patient_profile?.treatment_options,
           cpt_codes: aiRecommendations?.patient_profile?.cpt_codes,
           cpt_prompt_text: aiRecommendations?.patient_profile?.cpt_prompt_text,
+          diagnoses_prompt_text: aiRecommendations?.patient_profile?.diagnoses_prompt_text,
           search_query: aiRecommendations?.patient_profile?.search_query,
           searchOptions: searchOptions
         },
@@ -722,6 +723,7 @@ const HomePage: React.FC = () => {
             treatment_options: aiRecommendations?.patient_profile?.treatment_options,
             cpt_codes: aiRecommendations?.patient_profile?.cpt_codes,
             cpt_prompt_text: aiRecommendations?.patient_profile?.cpt_prompt_text,
+            diagnoses_prompt_text: aiRecommendations?.patient_profile?.diagnoses_prompt_text,
             search_query: aiRecommendations?.patient_profile?.search_query,
             searchOptions: searchOptions
           },
